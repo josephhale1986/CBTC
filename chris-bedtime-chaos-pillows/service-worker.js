389674a82f01a5960c5398134343de24e6229fb8
josephhale1986 <144393008+josephhale1986@users.noreply.github.com>
2025-08-09 22:48:04 -0400

    Delete chris-bedtime-chaos-pillows directory

--- chris-bedtime-chaos-pillows/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('cbc-v1').then(cache => cache.addAll([
-    './', './index.html', './game.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'
-  ])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
